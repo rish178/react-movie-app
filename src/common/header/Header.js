e2766f1a065b7fff95ff6456f03b73eb7b2ab943
rish178 <maheshwari.rishabh6@gmail.com>
2019-04-30 18:10:31 +0530

    set the header style

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,16 +1,21 @@
-import React , {Component} from 'react';
-import './Header.css';
-import Button from '@material-ui/core/Button';
+import React, { Component } from "react";
+import "./Header.css";
+import Button from "@material-ui/core/Button";
 
 class Header extends Component {
-
-    render(){
-        return(
-            <div>
-                <Button variant="contained" color="default">Log in</Button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <header className="app-header">
+          <div className="login-button">
+            <Button variant="contained" color="default">
+              Login
+            </Button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default Header
+export default Header;
